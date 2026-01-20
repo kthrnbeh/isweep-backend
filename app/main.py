7bd1a69e34ffbcd547b1b43a2565d10ce284eb5a
kthrnbeh <kthrnbeh@yahoo.com>
2026-01-19 19:40:11 -0700

    update

--- app/main.py
+++ app/main.py
@@ -38,9 +38,11 @@ def startup_event():
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"],
+    max_age=3600,
 )
 
 
